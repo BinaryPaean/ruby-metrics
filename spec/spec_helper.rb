c54ef93c4481601fd90455f63a052f5ef0b15282
Aaron Stewart <aaron@binarypaean.com>
2011-04-16 11:33:11 -0700

    Changes to make autotest happy for OSX & RVM users.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'simplecov'
 
 SimpleCov.start do
   add_filter "/spec/"
+  add_filter "/binstubs/" #Ignore binstubs folder, for RVM users
   add_group "Instruments", "lib/metrics/instruments"
   merge_timeout 3600
 end
